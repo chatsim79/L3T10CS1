1284a6553d341dad3e87de427e8cae01b4231605
chatsim79 <command?>
2022-12-15 13:20:44 +0000

    added documentation to L1T23CompTask/personalbio/urls.py

--- L1T23CompTask/personalbio/urls.py
+++ L1T23CompTask/personalbio/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 app_name = 'personalbio'
+"""
+The following URL pattern calls the 
+functions defined in views.py. in turn 
+these" URL patterns are called by the 
+project level urls file in the root 
+directory
+
+:param module views.home: home function in views.py:
+:param string 'home': name assigned to URL
+
+:rtype: rendered webpage
+"""
 urlpatterns = [
     path('home', views.home, name='home'),
 ]
