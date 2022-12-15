97bd681b617ce8a2d075d2aac0532f8240848fcd
chatsim79 <command?>
2022-12-15 13:40:13 +0000

    added documentation L1T23CompTask/userauth/forms.py

--- L1T23CompTask/userauth/forms.py
+++ L1T23CompTask/userauth/forms.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    """
+    Class for user object creation.
+    Uses default django forms module.
+    """
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     username = forms.CharField(max_length=30, required=True, help_text='Required')
     
     class Meta:
+        """
+        Class providing fields labels for form
+        """
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2', )
