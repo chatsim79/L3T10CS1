7869462b908bd0fcc877cf674610ffb2ee9f68ad
chatsim79 <command?>
2022-12-15 13:38:14 +0000

    added documentation to L1T23CompTask/shopping/views.py

--- L1T23CompTask/shopping/views.py
+++ L1T23CompTask/shopping/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 # Create your viedef index(request):
 def main(request):
+    """
+    This function, requests and renders the named 
+    html file
+
+    :param request: request command
+    :param main.html: html file to be rendered.
+
+    :rtype: returns rendered html
+    """    
     return render(request, "main.html")
